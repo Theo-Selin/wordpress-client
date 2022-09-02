0c57433b3d1b58bca8afd5dbcb3a17df41931152
Theo-Selin <theo.selin@hotmail.com>
2022-09-02 07:39:01 +0200

    alt property

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <div className='header'>
-        <img className='logo' src="https://logos.textgiraffe.com/logos/logo-name/Kissie-designstyle-love-heart-m.png"></img>
+        <img className='logo' alt="company logo" src="https://logos.textgiraffe.com/logos/logo-name/Kissie-designstyle-love-heart-m.png"></img>
         <h1 className='slogan'>Mitt liv som snygging</h1>
     </div>
   )
